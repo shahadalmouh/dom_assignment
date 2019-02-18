9957ab15158a49b34067ef39a5bd7cf96e514637
shahadalmouh <47005936+shahadalmouh@users.noreply.github.com>
2019-02-18 11:41:05 +0300

    don

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,4 +1,89 @@
-/*
-    Utilize the date class and also the css property rotate, float or flex
-    
-*/
+
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+
+function run_the_clock(){
+  var date = new Date();
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  
+  console.log("Hour: "+hr+ " Minute: "+ min + " Second: "+ sec);
+
+  let hrPosition = hr*360/12 + ((min * 360/60)/12) ;
+  let minPosition = (min * 360/60) + (sec* 360/60)/60;
+  let secPosition = sec * 360/60;
+
+  
+   HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+   MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+  
+}
+  
+
+var interval = setInterval(run_the_clock, 1000);
+////////////////////////////////
+
+setInterval(digitalClock, 1000)
+
+function digitalClock(){
+var currentTime = new Date ( );
+
+  var currentHours = currentTime.getHours ( );
+  var currentMinutes = currentTime.getMinutes ( );
+  var currentSeconds = currentTime.getSeconds ( );
+
+  // Pad the minutes and seconds with leading zeros, if required
+  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+  // Choose either "AM" or "PM" as appropriate
+  var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+
+  // Convert the hours component to 12-hour format if needed
+  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+
+  // Convert an hours component of "0" to "12"
+  currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+  // Compose the string for display
+  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+
+
+  // Update the time display
+  document.getElementById('txt').innerHTML =currentTimeString
+  //document.getElementById('txt2').innerHTML =currentTimeString
+  $(".clock").html(currentTimeString)
+  }
+  //////////////////
+  setInterval(digitalClock24, 1000)
+
+function digitalClock24(){
+var currentTime = new Date ( );
+
+  var currentHours = currentTime.getHours ( );
+  var currentMinutes = currentTime.getMinutes ( );
+  var currentSeconds = currentTime.getSeconds ( );
+
+  // Pad the minutes and seconds with leading zeros, if required
+   currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+
+
+
+  // // Convert an hours component of "0" to "12"
+   currentHours = ( currentHours == 0 ) ? 24 : currentHours;
+
+  // Compose the string for display
+  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds ;
+
+  
+  // Update the time display
+  document.getElementById('txt2').innerHTML =currentTimeString
+  $(".clock").html(currentTimeString)
+  }
+  //////////////////
